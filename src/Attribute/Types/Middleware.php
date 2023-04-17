368c384a828d60aa754431c975a18379d02e8917
Anoxia <anoxia@engineer.com>
2023-04-17 17:51:32 +0800

    modify: 补充注释

--- src/Attribute/Types/Middleware.php
+++ src/Attribute/Types/Middleware.php
@@ -14,7 +14,7 @@ class Middleware
      * @param string $name 中间件名称
      * @param string $desc 中间件说明
      * @param string $group 中间件分组
-     * @param int $priority 中间件优先级
+     * @param int $priority 中间件优先级，值越小优先级越高
      */
     public function __construct(
         public string $name = '',
