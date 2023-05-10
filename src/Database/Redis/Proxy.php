b2325eea3d3202033d05b2f772b65153ac097783
russell <qiang.wang@holla.world>
2023-05-10 17:43:14 +0800

    modify: code format

--- src/Database/Redis/Proxy.php
+++ src/Database/Redis/Proxy.php
@@ -5,7 +5,6 @@ namespace Library\Database\Redis;
 use function app;
 
 use Library\ConnectionPool;
-use PDO;
 
 class Proxy
 {
@@ -16,8 +15,8 @@ class Proxy
     /**
      * 动态代理 redis 方法
      *
-     * @param string $name redis 扩展所提供的所有方法名
-     * @param array $arguments 方法对应的参数
+     * @param string $name      redis 扩展所提供的所有方法名
+     * @param array  $arguments 方法对应的参数
      * @return mixed
      */
     public function __call($name, $arguments)
@@ -30,7 +29,7 @@ class Proxy
     /**
      * 以闭包方式执行 redis 操作
      * 传递 redis 连接给闭包作为参数
-     * 
+     *
      * @param callable $callback
      * @return mixed
      */
