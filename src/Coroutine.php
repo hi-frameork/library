eb688a82603b4dd12a4b6d9d315ba8e11d52ebc8
Russell <qiang.wang@holla.world>
2023-08-01 01:12:49 +0800

    modify: 优化 Coroutine 类,提高执行效率

--- src/Coroutine.php
+++ src/Coroutine.php
@@ -19,53 +19,24 @@ use Swoole\Coroutine as SwooleCoroutine;
  * Coroutine::attch($data);
  *
  * # 获取当前协程所在的根协程上挂载的数据
- * $data = Coroutine::getCtx();
+ * $data = Coroutine::getAttachData();
  * ```
  */
 class Coroutine extends SwooleCoroutine
 {
-    // /**
-    //  * 协程上下文映射
-    //  */
-    // protected static array $attches = [];
-
-    // /**
-    //  * 协程引用计数
-    //  */
-    // protected static array $referenceCount = [];
-
     /**
      * 创建协程
      */
     public static function create(callable $func, ...$params)
     {
         return parent::create(function () use ($func, $params) {
-            // 增加根协程引用计数
-            // 如果当前协程执行 attach，根协程需要减一
-            static::setReferenceCount(static::getCid(), false);
-            // 协程退出，清空关系映射
-            // Coroutine::defer(fn () => static::doDefer());
+            static::attch(
+                parent::getContext(static::getRpcid()) ?? null,
+                false
+            );
             // 执行回调业务
             call_user_func($func, ...$params);
         });
-    }
-
-    /**
-     * 为当前携程挂载数据
-     * 其将会通过 $maps 与 $referenceCount 在所有子协程中共享
-     */
-    public static function attch($data)
-    {
-        if (-1 === ($cid = static::getCid())) {
-            throw new RuntimeException('Runtime must be in swoole');
-        }
-
-        // 以当前协程 ID 作为根协程初始化引用计数
-        $pid = static::setReferenceCount($cid, true);
-        // 协程 ID 绑定数据
-        static::setCtx($pid, $data);
-        // 协程退出时执行清理
-        // Coroutine::defer(fn () => static::doDefer());
     }
 
     /**
@@ -73,17 +44,18 @@ class Coroutine extends SwooleCoroutine
      */
     protected static function getRpcid()
     {
-        if (false === ($pid = static::getCid())) {
+        if (false === ($pid = parent::getCid())) {
             throw new RuntimeException('Runtime must be in swoole');
         }
 
         // 循环向上查找，任何协程被 attach 即视为根协程
         while (true) {
-            if (static::getContext($pid)['referenceCount'] ?? 0) {
+            if (parent::getContext($pid)->attachRoot ?? 0) {
                 return $pid;
             }
 
             // 父协程 ID 小于 0，说明当前协程为根协程
+            // 跳出循环
             $id = static::getPcid($pid);
             if ($id <= 0) {
                 return $pid;
@@ -93,72 +65,39 @@ class Coroutine extends SwooleCoroutine
     }
 
     /**
-     * 创建协程 ID 映射
+     * 为当前携程挂载数据
+     * 其将会通过 $maps 与 $referenceCount 在所有子协程中共享
+     * 
+     * @param object|array $data
      */
-    protected static function setReferenceCount($cid, bool $isRoot = false)
+    public static function attch($data, bool $attachRoot = true)
     {
-        $rpid = static::getRpcid();
-        if ($isRoot) {
-            // 对于每一个新建的协程，默认会将其父协程的引用计数加一
-            // 所以此处需要找到当前协程根协程 ID，将其引用计数减一
-            if (static::getContext($rpid)['referenceCount'] ?? 0) {
-                parent::getContext($rpid)['referenceCount'] -= 1;
-            }
-
-            // 初始化根协程引用计数
-            parent::getContext($cid)['referenceCount'] = 1;
-
-            return $cid;
+        if (-1 === ($cid = parent::getCid())) {
+            throw new RuntimeException('Runtime must be in swoole');
         }
 
-        if (static::getContext($rpid)['referenceCount'] ?? 0) {
-            parent::getContext($rpid)['referenceCount'] += 1;
+        // if (null === $data) {
+        //     trigger_error('协程挂载目标数据不能为 null', E_USER_ERROR);
+        // }
+
+        $context = parent::getContext($cid);
+        // 标记当前节点是否为根节点
+        $context->attachRoot = $attachRoot;
+        // 对于根节点，挂载原始数据并创建自身引用，reference 为业务数据
+        // 对于普通节点根节点引用，reference 为根节点 context 引用
+        if ($attachRoot) {
+            $context->data = $data;
+            $context->reference = $context;
+        } else {
+            $context->reference = $data;
         }
-
-        return $rpid;
     }
 
     /**
-     * 获取指定协程 ID 的引用计数
+     * 获取当前协程所在的根协程上挂载的数据
      */
-    public static function getReferenceCount($cid)
+    public static function getAttachData()
     {
-        return parent::getContext($cid)['referenceCount'] ?? 0;
+        return parent::getContext(parent::getCid())->reference->data ?? null;
     }
-
-    /**
-     * 绑定数据
-     */
-    protected static function setCtx($id, $data)
-    {
-        parent::getContext($id)['data'] = $data;
-    }
-
-    /**
-     * 获取指定协程 ID 的上下文
-     * 如果未传入协程 ID，则默认获取当前协程指向的根协程上下文
-     */
-    public static function getCtx($cid = null)
-    {
-        $id = $cid ?? static::getRpcid();
-
-        return parent::getContext($id)['data'] ?? null;
-    }
-
-    /**
-     * 释放/清理协程上下文
-     */
-    // protected static function doDefer()
-    // {
-    //     $id = static::getRpcid();
-
-    //     // 减少引用计数
-    //     if (!empty(static::$referenceCount[$id])) {
-    //         static::$referenceCount[$id] -= 1;
-    //         // 检查并释放映射资源
-    //         if (static::$referenceCount[$id] <= 0) {
-    //             unset(static::$referenceCount[$id], static::$attches[$id]);
-    //         }
-    //     }
-    // }
 }
--- tests/Coroutine/CreateTest.php
+++ tests/Coroutine/CreateTest.php
@@ -6,6 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 class CreateTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        CoroutineStub::clear();
+    }
+
     public function testGetCid()
     {
         $this->assertSame(1, CoroutineStub::getCid());
@@ -21,13 +26,14 @@ class CreateTest extends TestCase
         CoroutineStub::create(function () {
             $this->assertSame(1, CoroutineStub::getPcid());
         });
+        //  预期不等，根携程 ID 为 1
         CoroutineStub::create(function () {
             $this->assertNotSame(2, CoroutineStub::getPcid());
         });
     }
 
     // 在没有 attach 数据的情况下，所有的协程根节点指向首个协程 ID
-    public function testGetRpcid()
+    public function testGetRpcidWithSigle()
     {
         $this->assertSame(1, CoroutineStub::getRpcid());
 
@@ -37,19 +43,12 @@ class CreateTest extends TestCase
         CoroutineStub::create(function () {
             $this->assertSame(1, CoroutineStub::getRpcid());
         });
-
         CoroutineStub::create(function () {
             $this->assertSame(1, CoroutineStub::getRpcid());
-            CoroutineStub::create(function () {
-                $this->assertSame(1, CoroutineStub::getRpcid());
-                CoroutineStub::create(function () {
-                    $this->assertSame(1, CoroutineStub::getRpcid());
-                });
-            });
         });
     }
 
-    public function testGetPrcideWithDefault()
+    public function testGetRpcidWithMultiNest()
     {
         CoroutineStub::create(function () {
             $this->assertSame(1, CoroutineStub::getRpcid());
@@ -67,97 +66,86 @@ class CreateTest extends TestCase
                 $this->assertSame(1, CoroutineStub::getRpcid());
                 CoroutineStub::create(function () {
                     $this->assertSame(1, CoroutineStub::getRpcid());
+                    CoroutineStub::create(function () {
+                        $this->assertSame(1, CoroutineStub::getRpcid());
+                        CoroutineStub::create(function () {
+                            $this->assertSame(1, CoroutineStub::getRpcid());
+                            CoroutineStub::create(function () {
+                                $this->assertSame(1, CoroutineStub::getRpcid());
+                            });
+                        });
+                    });
                 });
             });
         });
     }
 
-    // 没有执行数据挂载，所以引用计数为 0
-    public function testSetReferenceCountWithNoAttach()
+    // 在根节点挂载了 attach 数据的情况下，数据体为挂载的数据
+    public function testGetAttachDataWithSingleAttach()
     {
+        // 根携程未挂载数据，数据为 null
         CoroutineStub::create(function () {
-            $cid = CoroutineStub::getCid();
-            $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-            CoroutineStub::create(function () {
-                $cid = CoroutineStub::getCid();
-                $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-                CoroutineStub::create(function () {
-                    $cid = CoroutineStub::getCid();
-                    $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-                });
-            });
+            $this->assertSame(null, CoroutineStub::getAttachData());
         });
 
-        CoroutineStub::create(function () {
-            $cid = CoroutineStub::getCid();
-            $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-            CoroutineStub::create(function () {
-                $cid = CoroutineStub::getCid();
-                $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-                CoroutineStub::create(function () {
-                    $cid = CoroutineStub::getCid();
-                    $this->assertSame(0, CoroutineStub::getReferenceCount($cid));
-                });
-            });
-        });
-    }
-
-    public function testSetReferenceCountWithSigleLevelAttach()
-    {
-        $data = ['foo1' => 'bar1'];
+        // 根携程挂载数据
+        $data = ['foo' => 'bar'];
         CoroutineStub::attch($data);
-        $this->assertSame(1, CoroutineStub::getRpcid());
-        // $this->assertSame([1 => $data], CoroutineStub::getAttaches());
-        $this->assertSame(1, CoroutineStub::getReferenceCount(1));
-        $this->assertSame($data, CoroutineStub::getCtx());
-        // $this->assertSame([1 => 1], CoroutineStub::getAllReferenceCount());
+        $this->assertSame($data, CoroutineStub::getAttachData());
 
-        // 同一个协程，引用计数不变，后面数据覆盖前面数据
-        $data = ['foo2' => 'bar2'];
-        CoroutineStub::attch($data);
-        $this->assertSame(1, CoroutineStub::getRpcid());
-        // $this->assertSame([1 => $data], CoroutineStub::getAttaches());
-        $this->assertSame(1, CoroutineStub::getReferenceCount(1));
-        $this->assertSame($data, CoroutineStub::getCtx());
-        // $this->assertSame([1 => 1], CoroutineStub::getAllReferenceCount());
-    }
-
-    public function testSetReferenceCountWithMultiLevelAttach()
-    {
-        $data = ['foo1' => 'bar1'];
-        CoroutineStub::attch($data);
-        $this->assertSame(1, CoroutineStub::getRpcid());
-        // $this->assertSame([1 => $data], CoroutineStub::getAttaches());
-        $this->assertSame(1, CoroutineStub::getReferenceCount(1));
-        // $this->assertSame([1 => 1], CoroutineStub::getAllReferenceCount());
-        $this->assertSame($data, CoroutineStub::getCtx());
-
-        // 同一个协程，引用计数不变，后面数据覆盖前面数据
+        // 子协程默认继承父协程的挂载数据
         CoroutineStub::create(function () use ($data) {
-            // 在没有 attach 数据的情况下，协程根节点指向首个协程 ID
-            $this->assertSame(1, CoroutineStub::getRpcid());
+            $this->assertSame($data, CoroutineStub::getAttachData());
+        });
+    }
 
-            $cid = CoroutineStub::getCid();
-            $data2 = ['foo2' => 'bar2'];
-            CoroutineStub::attch($data2);
-            $this->assertSame($cid, CoroutineStub::getRpcid());
-            $this->assertSame(1, CoroutineStub::getReferenceCount(1));
-            $this->assertSame(1, CoroutineStub::getReferenceCount($cid));
-            $this->assertSame($data2, CoroutineStub::getCtx());
-            // $this->assertSame(
-            //     [
-            //         1 => 1,
-            //         $cid => 1,
-            //     ],
-            //     CoroutineStub::getAllReferenceCount()
-            // );
-            // $this->assertSame(
-            //     [
-            //         1 => $data,
-            //         $cid => $data2
-            //     ],
-            //     CoroutineStub::getAttaches()
-            // );
+    // 在根节点没有 attach 数据的情况下，数据体为 null
+    public function testGetAttachDataWithNoAttach()
+    {
+        $this->assertSame(null, CoroutineStub::getAttachData());
+    }
+
+    // 嵌套协程挂载情况
+    public function testGetAttachDataWithNestAttach()
+    {
+        CoroutineStub::create(function () {
+            // 还未挂载数据，数据为 null
+            $this->assertSame(null, CoroutineStub::getAttachData());
+
+            // 根携程挂载数据
+            $data = ['foo1' => 'bar1'];
+            CoroutineStub::attch($data);
+
+            CoroutineStub::create(function () use ($data) {
+                $this->assertSame($data, CoroutineStub::getAttachData());
+                CoroutineStub::create(function () use ($data) {
+                    $this->assertSame($data, CoroutineStub::getAttachData());
+                    CoroutineStub::create(function () use ($data) {
+                        $this->assertSame($data, CoroutineStub::getAttachData());
+                        CoroutineStub::create(function () use ($data) {
+                            $this->assertSame($data, CoroutineStub::getAttachData());
+                        });
+                    });
+                });
+
+                CoroutineStub::create(function () {
+                    $data = ['foo2' => 'bar2'];
+                    CoroutineStub::attch($data);
+                    $this->assertSame($data, CoroutineStub::getAttachData());
+                });
+
+                CoroutineStub::create(function () {
+                    $data = ['foo3' => 'bar3'];
+                    CoroutineStub::attch($data);
+                    $this->assertSame($data, CoroutineStub::getAttachData());
+                });
+            });
+
+            CoroutineStub::create(function () {
+                $data = ['foo4' => 'bar4'];
+                CoroutineStub::attch($data);
+                $this->assertSame($data, CoroutineStub::getAttachData());
+            });
         });
     }
 }
@@ -167,6 +155,12 @@ class CoroutineStub extends \Library\Coroutine
     public static function getRpcid()
     {
         return parent::getRpcid();
+    }
+
+    public static function clear()
+    {
+        $context = parent::getContext();
+        unset($context->data, $context->reference);
     }
 
     // public static function getAttaches()
