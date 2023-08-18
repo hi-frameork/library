7f04508b55234323cbe500274258a79baf5454b1
wangqiang <wangqiang@typing.email>
2023-08-18 10:27:17 +0800

    modify: 增加 clickhouse 启动配置

--- tests/Suites/Database/ClickHouse/ManagerTest.php
+++ tests/Suites/Database/ClickHouse/ManagerTest.php
@@ -4,4 +4,9 @@ namespace Tests\Suites\Database\ClickHouse;
 
 class ManagerTest extends TestCase
 {
+    public function testCreate()
+    {
+        $pool = $this->manager->pool('default');
+        $this->assertInstanceOf(\ClickHouseDB\Client::class, $pool->get());;
+    }
 }
--- tests/Suites/Database/ClickHouse/TestCase.php
+++ tests/Suites/Database/ClickHouse/TestCase.php
@@ -2,8 +2,17 @@
 
 namespace Tests\Suites\Database\ClickHouse;
 
+use Library\Database\ClickHouse\Manager;
 use PHPUnit\Framework\TestCase as FrameworkTestCase;
 
 abstract class TestCase extends FrameworkTestCase
 {
+    protected Manager $manager;
+
+    protected function setUp(): void
+    {
+        $this->manager = new Manager([
+            'default' => config('clickhouse.warehouse'),
+        ]);
+    }
 }
