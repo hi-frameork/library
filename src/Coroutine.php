4e23b1f1cefef1138da259565e78493f335a1a7b
anoxia <anoxia@engineer.com>
2024-07-19 16:07:51 +0800

    modify: 优化协程上下文数据传递

--- src/Coroutine.php
+++ src/Coroutine.php
@@ -2,9 +2,8 @@
 
 namespace Library;
 
-use RuntimeException;
+use ArrayAccess;
 use Swoole\Coroutine as SwooleCoroutine;
-use Throwable;
 
 /**
  * 协程上下文，用于在协程间共享数据
@@ -23,86 +22,92 @@ use Throwable;
  * $data = Coroutine::getAttachData();
  * ```
  */
-class Coroutine extends SwooleCoroutine
+class Coroutine
 {
     /**
      * 创建协程
+     *
+     * @param callable          $fn
+     * @param array<int, mixed> $params
      */
-    public static function create(callable $func, ...$params): int|false
+    public static function create(callable $fn, ...$params): mixed
     {
-        return SwooleCoroutine::create(function () use ($func, $params) {
-            try {
-                static::attch(
-                    parent::getContext(static::getRpcid()) ?? null,
-                    false
-                );
-                // 执行回调业务
-                $func(...$params);
-            } catch (Throwable $th) {
-                error($th->getMessage(), $th->getTrace());
-            }
-        });
-    }
-
-    /**
-     * 返回当前协程父 ID 映射的根协程 ID
-     */
-    protected static function getRpcid()
-    {
-        if (false === ($pid = parent::getCid())) {
-            throw new RuntimeException('Runtime must be in swoole');
-        }
-
-        // 循环向上查找，任何协程被 attach 即视为根协程
-        for (;;) {
-            if (parent::getContext($pid)->attachRoot ?? 0) {
-                return $pid;
-            }
-
-            // 父协程 ID 小于 0，说明当前协程为根协程
-            // 跳出循环
-            $id = static::getPcid($pid);
-            if ($id <= 0) {
-                return $pid;
-            }
-            $pid = $id;
-        }
+        return SwooleCoroutine::create(
+            fn ($fn, $params, $data) => Coroutine::setAttach($data) && tryCatch($fn, $params, false),
+            $fn,
+            $params,
+            static::getAttach()
+        );
     }
 
     /**
      * 为当前携程挂载数据
      * 其将会通过 $maps 与 $referenceCount 在所有子协程中共享
      *
-     * @param object|array $data
+     * @deprecated 见 setAttach() 方法
      */
-    public static function attch($data, bool $attachRoot = true)
+    public static function attch(mixed $data, string $name = '__data'): bool
     {
-        if (-1 === ($cid = parent::getCid())) {
-            throw new RuntimeException('Runtime must be in swoole');
-        }
+        return static::setAttach($data, $name);
+    }
 
-        // if (null === $data) {
-        //     trigger_error('协程挂载目标数据不能为 null', E_USER_ERROR);
-        // }
+    /**
+     * 为当前携程挂载数据
+     * 其将会通过 $maps 与 $referenceCount 在所有子协程中共享
+     */
+    public static function setAttach(mixed $data, string $name = '__data'): bool
+    {
+        $context = static::getContext();
 
-        $context = parent::getContext($cid);
-        // 标记当前节点是否为根节点
-        $context['attachRoot'] = $attachRoot;
-        // 对于根节点，挂载原始数据并创建自身引用，reference 为业务数据
-        // 对于普通节点根节点引用，reference 为根节点 context 引用
-        if ($attachRoot) {
-            $context['data']      = $data;
-            $context['reference'] = $context;
+        if ($data === null) {
+            unset($context[$name]);
         } else {
-            $context['reference'] = $data;
+            $context[$name] = $data;
         }
+
+        return true;
+    }
+
+    /**
+     * @return mixed
+     * @deprecated 见 getAttach() 方法
+     */
+    public static function getAttachData()
+    {
+        return static::getAttach();
     }
 
     /**
      * 获取当前协程所在的根协程上挂载的数据
      */
-    public static function getAttachData()
+    public static function getAttach(string $name = '__data'): mixed
     {
-        return parent::getContext(parent::getCid())['reference']['data'] ?? null;
+        return static::getContext()[$name] ?? null;
+    }
+
+    /**
+     * @return ArrayAccess
+     */
+    public static function getContext(): object
+    {
+        return SwooleCoroutine::getContext();
+    }
+
+    /**
+     * @param int|float $seconds
+     * @return mixed
+     */
+    public static function sleep(int|float $seconds)
+    {
+        return SwooleCoroutine::sleep($seconds);
+    }
+
+    /**
+     * @param mixed $callback
+     * @return mixed
+     */
+    public static function defer(callable $callback)
+    {
+        return SwooleCoroutine::defer($callback);
     }
 }
