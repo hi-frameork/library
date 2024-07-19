68761e0aa2d8c076944e5334d1db6d9bf749acc3
anoxia <anoxia@engineer.com>
2024-07-19 13:53:34 +0800

    modify: 优化 redis 短线重连处理，修正 subscribe 超时问题

--- src/Database/Redis/Proxy.php
+++ src/Database/Redis/Proxy.php
@@ -8,36 +8,51 @@ use Library\ConnectionPool;
 
 class Proxy
 {
-    public function __construct(private string $connection, private int $failedRetries = 3)
+    public function __construct(private string $connection)
     {
     }
 
     /**
-     * @param array<string,mixed> $arguments
+     * 动态代理 redis 方法
+     *
+     * @param string $name      redis 扩展所提供的所有方法名
+     * @param array  $arguments 方法对应的参数
+     * @return mixed
      */
-    public function __call(string $name, array $arguments): mixed
+    public function __call($name, $arguments)
     {
-        /** @var ConnectionPool $pool */
-        $pool = app('db.pool.redis')->pool($this->connection);
+        debug('REDIS', [$name, $arguments]);
 
-        $hasError = false;
-        $throw = null;
-        for ($i = 0; $i < $this->failedRetries; $i++) {
-            /** @var \Redis $redis */
-            $redis = $pool->get();
-            try {
-                debug('REDIS', [$name, $arguments]);
-                return $redis->{$name}(...$arguments);
-            } catch (\Throwable $th) {
-                $hasError = true;
-                $throw = $th;
-            } finally {
-                if ($hasError === false) {
-                    $pool->put($redis);
-                }
-            }
+        return $this->builtIn(
+            fn ($redis) => $redis->{$name}(...$arguments)
+        );
+    }
+
+    /**
+     * 以闭包方式执行 redis 操作
+     * 传递 redis 连接给闭包作为参数
+     *
+     * @param callable $callback
+     * @return mixed
+     */
+    public function builtIn(callable $callback)
+    {
+        /** @var \Library\Database\Manager $manager */
+        $manager = app('db.pool.redis');
+        /** @var ConnectionPool $pool */
+        $pool = $manager->pool($this->connection);
+
+        /** @var \Redis $redis */
+        $redis = $pool->get();
+
+        try {
+            $result = $callback($redis);
+        } catch (\Throwable $th) {
+            throw $th;
+        } finally {
+            $pool->put($redis);
         }
 
-        throw $throw;
+        return $result;
     }
 }
